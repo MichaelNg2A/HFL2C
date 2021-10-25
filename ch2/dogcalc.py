970bce850434fcdbeeca9bebf4a1e731bedfcaa8
MichaelNg2A <MichaelNg2A@users.noreply.github.com>
2021-10-24 18:01:30 -0700

    Changed formatting to match the book example and then corrected my Semantic error of referencing the wrong variable.

--- ch2/dogcalc.py
+++ ch2/dogcalc.py
@@ -1,4 +1,8 @@
 dog_name = input("What is your dog's name? ")
 dog_age = int(input("What is your dog's age? "))
 human_age = dog_age * 7
-print('Your Dog', dog_name, 'is', dog_age, 'years old in human years')
+print('Your dog',
+      dog_name,
+      'is',
+      human_age,
+      'years old in human years')
