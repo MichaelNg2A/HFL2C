d8426507141c5287b5cb3882b6ed575dfa5bde12
MichaelNg2A <MichaelNg2A@users.noreply.github.com>
2021-10-24 17:59:22 -0700

    Corrected syntax error in print statement and tightened up formatting.

--- ch2/dogcalc.py
+++ ch2/dogcalc.py
@@ -1,4 +1,4 @@
 dog_name = input("What is your dog's name? ")
 dog_age = int(input("What is your dog's age? "))
 human_age = dog_age * 7
-print('Your Dog ' + dog_name + ' is ' + dog_age ' years old in human years')
+print('Your Dog', dog_name, 'is', dog_age, 'years old in human years')
