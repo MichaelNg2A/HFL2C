bacf8173a3c95d7ccdc5a46a40ca15a1334516b4
MichaelNg2A <MichaelNg2A@users.noreply.github.com>
2021-11-01 20:56:38 -0700

    Added Original subroutine to determine if the Computer wins the game.

--- ch3/rock.py
+++ ch3/rock.py
@@ -1,7 +1,26 @@
 import random
 
+winner = ''
+
 choices =  ['rock', 'paper', 'scissors']
 computer_choice  =  random.choice(choices)
 
 user_choice = input('rock, paper or scissors? ')
 print('You chose', user_choice, 'and the computer chose', computer_choice)
+
+if computer_choice == user_choice:
+    winner = 'Tie'
+
+elif computer_choice == 'paper' and user_choice == 'rock':
+    winner = 'Computer'
+
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    winner = 'Computer'
+
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    winner = 'Computer'
+
+else:
+    winner = 'User'
+
+print(winner)
