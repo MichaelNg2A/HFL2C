611f081a6df055b33ca3fae4db38d15d11aa7136
MichaelNg2A <MichaelNg2A@users.noreply.github.com>
2021-11-01 20:59:48 -0700

    Escaped the CR/LF on the Computer Winning subroutine to make it more readable to Humans.

--- ch3/rock.py
+++ ch3/rock.py
@@ -11,7 +11,9 @@ print('You chose', user_choice, 'and the computer chose', computer_choice)
 if computer_choice == user_choice:
     winner = 'Tie'
 
-elif (computer_choice == 'paper' and user_choice == 'rock') or (computer_choice == 'rock' and user_choice == 'scissors') or (computer_choice == 'scissors' and user_choice == 'paper'):
+elif (computer_choice == 'paper' and user_choice == 'rock') \
+     or (computer_choice == 'rock' and user_choice == 'scissors') \
+     or (computer_choice == 'scissors' and user_choice == 'paper'):
     winner = 'Computer'
 
 else:
