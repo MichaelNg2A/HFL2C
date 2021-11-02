613085492b1421b93a813e007e7446d450c660c4
MichaelNg2A <MichaelNg2A@users.noreply.github.com>
2021-11-01 21:31:43 -0700

    Added subroutine to test for Valid User Input, added Comments, Cleaned up escaped line breaks.

--- ch3/rock.py
+++ ch3/rock.py
@@ -1,24 +1,68 @@
+#!/usr/bin/env python3
+############################################################
+# rock.py                                                  #
+############################################################
+#                                                          #
+# Name:       rock.py                                      #
+# Version:    1.1                                          #
+# Updated:    November 1st, 2021                           #
+# Author:     MichaelNg.GitHub@KWMeads.com                 #
+#                                                          #
+# This is a Python Program to play Rock, Paper Scissors    #
+# with the computer.  Based on the exercises in Head First #
+# Learn to Code - Chapter #3.  Good Luck!                  #
+#                                                          #
+############################################################
+
 import random
+
+# Define the "winner" variable with an empty string.
 
 winner = ''
 
+# Define the options that the computer can choose from.
+
 choices =  ['rock', 'paper', 'scissors']
+
+# Use the random.choice module to randomly choose one of the options above.
+
 computer_choice  =  random.choice(choices)
 
-user_choice = input('rock, paper or scissors? ')
+# Subroutine to prompt the user for an option and verify that it's valid.
+
+user_choice = ''
+while (user_choice != 'rock' and
+    user_choice != 'paper' and
+    user_choice != 'scissors'):
+        user_choice = input('rock, paper or scissors? ')
+
+# Display what everyone's choices were.
+
+print('You choose', user_choice, 'and the computer choose', computer_choice)
+
+#  Define the "winner" if there's a tie.
 
 if computer_choice == user_choice:
     winner = 'Tie'
 
-elif (computer_choice == 'paper' and user_choice == 'rock') \
-     or (computer_choice == 'rock' and user_choice == 'scissors') \
-     or (computer_choice == 'scissors' and user_choice == 'paper'):
+# elif line with nested logic to walk through all the other scenarios where the Computer wins.
+
+elif ((computer_choice == 'paper' and user_choice == 'rock') 
+     or (computer_choice == 'rock' and user_choice == 'scissors') 
+     or (computer_choice == 'scissors' and user_choice == 'paper')):
     winner = 'Computer'
+
+# If the computer is not the winner, the User must be the winner.
 
 else:
     winner = 'User'
 
+# Use this wording on the winner line if there's a tie.
+
 if winner == 'Tie':
-    print('We both chose', computer_choice +', play again.')
+    print('We both choose', computer_choice + ', play again.')
+
+# If there's only one winner, use the original wording to announce it.
+
 else:
-    print(winner, 'won, I chose', computer_choice +'.')
+    print(winner, 'won, I choose', computer_choice + '.')
